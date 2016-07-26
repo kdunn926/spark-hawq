8f5a7b34cf93a72a2639a06238acee7b98b507f9
Kyle Dunn <kdunn926@gmail.com>
2016-07-26 16:49:12 -0600

    Generalizing the example a bit

--- src/main/scala/SparkHawqApp.scala
+++ src/main/scala/SparkHawqApp.scala
@@ -16,15 +16,29 @@ object SparkHawqApp {
     // Create a new SparkContext using the configuration
     val sc = new SparkContext(conf)
 
-    val hadoopConf1 = SparkHadoopUtil.get.newConfiguration();
+    val hadoopConf = SparkHadoopUtil.get.newConfiguration();
+
+    // The HAWQ test table
+    //   postgres=# \d+ sparkTest                                                                                                         Append-Only Table "public.sparktest"
+    //    Column  |  Type   | Modifiers | Storage  | Description
+    //   ---------+---------+-----------+----------+-------------
+    //    myid    | integer |           | plain    |
+    //    myvalue | text    |           | extended |
+    //   Compression Type: None
+    //   Compression Level: 0
+    //   Block Size: 32768
+    //   Checksum: f
+    //   Has OIDs: no
+    //   Options: appendonly=true
+    //   Distributed randomly
 
     // Tell HAWQInputFormat where the HAWQ master is, which user to use, the schema, and the table
-    HAWQInputFormat.setInput(hadoopConf1, "sandbox.hortonworks.com:10432/postgres", "gpadmin", "public", "test")
+    HAWQInputFormat.setInput(hadoopConf, "sandbox.hortonworks.com:10432/postgres", "gpadmin", "public", "sparktest")
 
     // Create an RDD from the HAWQ table -- _._2 refers to the value (classOf[HAWQRecord]) member of the RDD
-    val rdd1 = sc.newAPIHadoopRDD(hadoopConf1, classOf[HAWQInputFormat], classOf[Void], classOf[HAWQRecord]).map(_._2);
+    val rdd = sc.newAPIHadoopRDD(hadoopConf, classOf[HAWQInputFormat], classOf[Void], classOf[HAWQRecord]).map(_._2);
 
-    // Cast the result set to a string and print each item
-    val employees = rdd1.map(p => (p.getString(1))).collect().foreach(println);
+    // Cast the result set (myId -> int, myValue -> string) and print each tuple
+    rdd.map(row => (row.getInt(1), row.getString(2))).collect().foreach(println);
   }
 }
